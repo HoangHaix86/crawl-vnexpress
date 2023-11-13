1cd081eecce3a631446b871096c96e368b8634e7
HoangHaix86 <hai.hoang.2762@gmai.com>
2023-11-13 15:18:22 +0700

    update script

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+cd vnexpress
 scrapy crawl vnexpress -o dataset.json
--- vnexpress/VNExpress/spiders/vnexpress-spider.py
+++ vnexpress/VNExpress/spiders/vnexpress-spider.py
@@ -10,9 +10,9 @@ class VNExpressSpider(CrawlSpider):
     start_urls = ["https://vnexpress.net"]
 
     custom_settings = {
-        "CONCURRENT_ITEMS": 10,
-        "CONCURRENT_REQUESTS": 5,
-        "CONCURRENT_REQUESTS_PER_DOMAIN": 5,
+        "CONCURRENT_ITEMS": 20,
+        "CONCURRENT_REQUESTS": 10,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 10,
         "DEPTH_PRIORITY" : 1 ,
         "SCHEDULER_DISK_QUEUE" : 'scrapy.squeues.PickleFifoDiskQueue',
         "SCHEDULER_MEMORY_QUEUE" : 'scrapy.squeues.FifoMemoryQueue'
