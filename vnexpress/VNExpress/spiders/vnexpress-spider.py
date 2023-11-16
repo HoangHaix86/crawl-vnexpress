90073a701b1f776fd00ef6eb5ab3094acb580c51
HoangHaix86 <hai.hoang.2762@gmai.com>
2023-11-16 14:28:51 +0700

    update

--- vnexpress/VNExpress/spiders/vnexpress-spider.py
+++ vnexpress/VNExpress/spiders/vnexpress-spider.py
@@ -8,9 +8,9 @@ class VNExpressSpider(CrawlSpider):
     start_urls = ["https://vnexpress.net"]
 
     custom_settings = {
-        "CONCURRENT_ITEMS": 10,
-        "CONCURRENT_REQUESTS": 10,
-        "CONCURRENT_REQUESTS_PER_DOMAIN": 10,
+        "CONCURRENT_ITEMS": 30,
+        "CONCURRENT_REQUESTS": 30,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 30,
         "DEPTH_PRIORITY" : 1 ,
         "SCHEDULER_DISK_QUEUE" : 'scrapy.squeues.PickleFifoDiskQueue',
         "SCHEDULER_MEMORY_QUEUE" : 'scrapy.squeues.FifoMemoryQueue'
@@ -29,7 +29,7 @@ class VNExpressSpider(CrawlSpider):
             )
     ]
     
-    conn = sqlite3.connect('../../data.db')
+    conn = sqlite3.connect('/content/drive/MyDrive/data.db')
     c = conn.cursor()
     
     def __init__(self, *a, **kw):
